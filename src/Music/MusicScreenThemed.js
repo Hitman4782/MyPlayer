27815002dcef4f5fdb61af069f5800e5e1f8bba7
Zuhire <hitman4782@gmail.com>
2023-08-02 02:45:31 +0500

    bug fixes

--- src/Music/MusicScreenThemed.js
+++ src/Music/MusicScreenThemed.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { useTheme } from '../../components/ThemeContext';
 import AudioListScreen from './Music'; // Adjust the import path as needed
 
-const AudioListScreenWithTheme = ({ navigation }) => {
+const AudioListScreenWithTheme = ({ navigation, route }) => {
   const { theme } = useTheme();
 
-  return <AudioListScreen {...{ theme, navigation }} />;
+  return <AudioListScreen {...{ theme, navigation, route }} />;
 };
 
 export default AudioListScreenWithTheme;
